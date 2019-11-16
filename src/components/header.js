0f0278f749cd7be1273342a06b1a0f19d18baf25
Jack'tnp <60070031@it.kmitl.ac.th>
2019-11-17 03:27:45 +0700

    commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#993333`,
+      background: `#fbbe00`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 const PostLink = ({ post }) => (
-    <div class="container bg-dark">
+    <div class="text-danger">
         <Link to={post.frontmatter.path}>
             {post.frontmatter.title}({post.frontmatter.date})
         </Link>
